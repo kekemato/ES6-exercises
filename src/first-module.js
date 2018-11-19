1e642c08d0a570d237dd16d8d63ad8de6335709b
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-19 11:13:18 +0100

    scopes of variables in modules

---
+++ src/first-module.js
@@ -0,0 +1,9 @@
+let counter = 0
+
+const inc = () => counter++
+
+console.log('Counter initilized value is: ', counter)
+
+inc()
+
+console.log('Counter value is now: ', counter)
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './first-module'
+import './first-module'
+import './first-module'
+
+const Puszek = "Puszek <3"
+
+console.log(Puszek)
+
+
+console.log(this)
+
+console.log(counter)
